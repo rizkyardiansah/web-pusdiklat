cb66a76ad32e9ffc9c82bf1502defc9a559ac3e6
Albert <albertlim220800@gmail.com>
2021-07-25 23:43:21 +0700

    membuat tampilan ui admin pusat

---
+++ application/views/administrator-pusat/menu_rejection.php
@@ -0,0 +1,160 @@
+		<!-- side content -->
+		<div id="layoutSidenav_content">
+			<main>
+				<div class="container-fluid px-4">
+					<h1 class="mt-4">Menu Persetujuan</h1>
+					<ol class="breadcrumb mb-4">
+						<li class="breadcrumb-item active">Data yang tidak disetujui</li>
+					</ol>
+					<div class="row">
+						<div class="col-xl-3 col-md-6">
+							<div class="card bg-primary text-white mb-4">
+								<div class="card-body">14 Total data pelamar</div>
+							</div>
+						</div>
+						<div class="col-xl-3 col-md-6">
+							<div class="card bg-danger text-white mb-4">
+								<div class="card-body">15 Data telah ditolak</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xl-3 col-md-6">
+							<div class="card bg-success text-white mb-4">
+								<select class="form-select form-select-sm" aria-label=".form-select-sm example">
+									<option selected>Open this select menu</option>
+									<option value="1">Biro Perencanaan dan Keuangan</option>
+									<option value="2">Biro Hukum, Organisasi, Kerja Sama dan Hubungan Masyarakat</option>
+									<option value="3">Biro SDM dan Umum</option>
+									<option value="4">Direktorat Deposit dan Pengembangan Koleksi Perpustakaan</option>
+									<option value="5">Pusat Bibliografi dan Pengolahan Bahan Perpustakaan</option>
+									<option value="6">Pusat Preservasi dan Alih Media Bahan Perpustakaan</option>
+									<option value="7">Pusat Jasa Informasi Perpustakaan dan Pengelolaan Naskah Nusantara</option>
+									<option value="8">Deputi Bidang Pengembangan Sumber Daya Perpustakaan</option>
+									<option value="9">Pusat Pengembangan Perpustakaan Umum dan Khusus</option>
+									<option value="10">Pusat Pengembangan Perpustakaan Sekolah/Madrasah dan Perguruan Tinggi</option>
+									<option value="11">Pusat Analisis Perpustakaan dan Pengembangan Budaya Baca</option>
+									<option value="12">Pusat Data dan Informasi</option>
+									<option value="13">Pusat Pembinaan Pustakawan</option>
+									<option value="14">Pusat Pendidikan dan Pelatihan</option>
+									<option value="15">Inspektorat</option>
+								</select>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<table class="table table-hover">
+								<thead>
+									<tr>
+										<th scope="col">No</th>
+										<th scope="col">Nama Lengkap</th>
+										<th scope="col">Instansi</th>
+										<th scope="col" class="text-center">Unit Kerja</th>
+										<th scope="col">Kelengkapan Berkas</th>
+										<th scope="col">Keterangan</th>
+										<th scope="col" class="text-center">Aksi</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<th scope="row">1</th>
+										<td>Mark</td>
+										<td>Otto</td>
+										<td>Pusat Data dan Informasi</td>
+										<td>
+											Berkas CV.pdf
+											<br>
+											Berkas khs.pdf
+											<br>
+											Nama surat magang.pdf
+										</td>
+										<td> Berkas Tidak Memenuhi Syarat</td>
+										<td colspan='2' class="text-center">
+											<button class="btn btn-sm  btn-primary" type="button"><i class="fa fa-edit" aria-hidden="true"></i>
+											Cetak Surat
+											</button>
+											</td>	
+									</tr>
+									<tr>
+										<th scope="row">2</th>
+										<td>Jacob</td>
+										<td>Thornton</td>
+										<td>Pusat Pengambangan Perpustakaan Umum dan Khusus</td>
+										<td>
+											Berkas CV.pdf
+											<br>
+											Berkas khs.pdf
+											<br>
+											Nama surat magang.pdf
+										</td>
+										<td> Berkas Tidak Memenuhi Syarat</td>
+										<td colspan='2' class="text-center">
+											<button class="btn btn-sm  btn-primary" type="button"><i class="fa fa-edit" aria-hidden="true"></i>
+											Cetak Surat
+											</button>
+											</td>	
+									</tr>
+									<tr>
+										<th scope="row">3</th>
+										<td>Larry the Bird</td>
+										<td>@twitter</td>
+										<td>Pusat Analisis Perpustakaan dan Pengembangan Budaya Baca</td>
+										<td>
+											Berkas CV.pdf
+											<br>
+											Berkas khs.pdf
+											<br>
+											Nama surat magang.pdf
+										</td>
+										<td> Berkas Tidak Memenuhi Syarat</td>
+										<td colspan='2' class="text-center">
+											<button class="btn btn-sm  btn-primary" type="button"><i class="fa fa-edit" aria-hidden="true"></i>
+												Cetak Surat
+											</button>
+											</td>	
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+					<!-- pagination section -->
+					<div class="row">
+						<nav aria-label="Page navigation example">
+							<ul class="pagination">
+								<li class="page-item">
+									<a class="page-link" href="#" aria-label="Previous">
+										<span aria-hidden="true">&laquo;</span>
+										<span class="sr-only">Previous</span>
+									</a>
+								</li>
+								<li class="page-item"><a class="page-link" href="#">1</a></li>
+								<li class="page-item"><a class="page-link" href="#">2</a></li>
+								<li class="page-item"><a class="page-link" href="#">3</a></li>
+								<li class="page-item">
+									<a class="page-link" href="#" aria-label="Next">
+										<span aria-hidden="true">&raquo;</span>
+										<span class="sr-only">Next</span>
+									</a>
+								</li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</main>
+			<footer class="py-4 bg-light mt-auto">
+				<div class="container-fluid px-4">
+					<div class="d-flex align-items-center justify-content-between small">
+						<div class="text-muted">Copyright &copy; Your Website 2021</div>
+						<div>
+							<a href="#">Privacy Policy</a>
+							&middot;
+							<a href="#">Terms &amp; Conditions</a>
+						</div>
+					</div>
+				</div>
+			</footer>
+
+		</div>
+
+		</div>
