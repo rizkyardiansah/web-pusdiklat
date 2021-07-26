1517c7d9eb2ca864472e693960bdc7d01381334c
rizkyardiansah <muhammadrizkyardiansah93@gmail.com>
2021-07-26 11:30:25 +0700

    menyempurnakan page profile

--- application/views/pelamar/profile.php
+++ application/views/pelamar/profile.php
@@ -7,7 +7,7 @@
 
             <div class="row" data-aos="fade-up" data-aos-delay="100">
                 <div class="col-lg-5">
-                    <ul class="nav nav-tabs flex-column">
+                    <ul class="nav nav-tabs flex-column profile">
                         <li>
                             <div class="row">
                                 <div class="col-lg-3"><img src="<?= base_url('assets/img/') . 'default.jpg'; ?>" class="img-thumbnail" style="height: 5rem;"></div>
@@ -19,29 +19,29 @@
                             <br>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active show" data-toggle="tab" href="#tab-1">Informasi Pribadi</a>
+                            <a class="nav-link active show" data-toggle="tab" id="button-1" href="#tab-1">Informasi Pribadi</a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="#tab-2">Informasi Akademik</a>
+                            <a class="nav-link" data-toggle="tab" id="button-2" href="#tab-2">Informasi Akademik</a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="#tab-3">Berkas</a>
+                            <a class="nav-link" data-toggle="tab" id="button-3" href="#tab-3">Berkas</a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="#tab-4">Akun</a>
+                            <a class="nav-link" data-toggle="tab" id="button-4" href="#tab-4">Akun</a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="#tab-5">Keluar</a>
+                            <a class="nav-link" data-toggle="tab" id="button-5" href="#tab-5">Keluar</a>
                         </li>
 
                     </ul>
                 </div>
                 <div class="col-lg-7 mt-4 mt-lg-0">
-                    <div class="tab-content">
+                    <div class="tab-content profile">
                         <div class="tab-pane active show" id="tab-1">
                             <div class="row">
                                 <div class="col-lg-10 details order-2 order-lg-1">
@@ -50,7 +50,8 @@
                                     <form action="" method="POST">
                                         <div class="form-group">
                                             <label class="label" for="nama">Nama</label>
-                                            <input class="form-control" type="text" id="nama" name="nama" value="<?= $user['nama']; ?>">
+                                            <input class="form-control" type="text" id="nama" name="nama" value="<?= form_error('nama') || set_value('nama') ? set_value('nama') : $user['nama']; ?>">
+                                            <?= form_error('nama', '<small class="text-danger">', '</small>'); ?>
                                         </div>
 
                                         <div class="form-group">
@@ -59,41 +60,46 @@
                                         </div>
                                         <div class="form-group">
                                             <label class="label" for="no_telp">No.Telp/WhatsApp</label>
-                                            <input class="form-control" type="text" id="no_telp" name="no_telp" value="<?= $user['no_telpon']; ?>">
+                                            <input class="form-control" type="text" id="no_telp" name="no_telp" value="<?= form_error('no_telp') || set_value('no_telp') ? set_value('no_telp') : $user['no_telpon']; ?>">
+                                            <?= form_error('no_telp', '<small class="text-danger">', '</small>'); ?>
                                         </div>
-                                        <button class="btn btn-primary float-right">Simpan perubahan</button>
+                                        <button class="btn btn-primary float-right" name="submitInfoPribadi" id="submitInfoPribadi" value="submitInfoPribadi">Simpan perubahan</button>
                                     </form>
                                 </div>
                             </div>
                         </div>
                         <div class="tab-pane" id="tab-2">
                             <div class="row">
-                                <div class="col-lg-8 details order-2 order-lg-1">
+                                <div class="col-lg-10 details order-2 order-lg-1">
                                     <h3>Informasi Akademik</h3>
                                     <!-- <hr> -->
-                                    <form action="" method="POST">
+                                    <form action="" method="POST" id="infoAkad">
                                         <div class="form-group">
                                             <label class="label" for="universitas">Universitas</label>
-                                            <input class="form-control" type="text" id="universitas" name="universitas" value="<?= $user['universitas']; ?>">
+                                            <input class="form-control" type="text" id="universitas" name="universitas" value="<?= form_error('universitas') || set_value('universitas') ? set_value('universitas') : $user['universitas']; ?>">
+                                            <?= form_error('universitas', '<small class="text-danger">', '</small>'); ?>
                                         </div>
 
                                         <div class="form-group">
                                             <label class="label" for="nim">Nomor Induk Mahasiswa</label>
-                                            <input class="form-control" type="text" id="nim" name="nim" value="<?= $user['nim']; ?>">
+                                            <input class="form-control" type="text" id="nim" name="nim" value="<?= form_error('nim') || set_value('nim') ? set_value('nim') : $user['nim']; ?>">
+                                            <?= form_error('nim', '<small class="text-danger">', '</small>'); ?>
                                         </div>
                                         <div class="form-group">
                                             <label class="label" for="fakultas">Fakultas</label>
-                                            <input class="form-control" type="text" id="fakultas" name="fakultas" value="<?= $user['fakultas']; ?>">
+                                            <input class="form-control" type="text" id="fakultas" name="fakultas" value="<?= form_error('fakultas') || set_value('fakultas') ? set_value('fakultas') : $user['fakultas']; ?>">
+                                            <?= form_error('fakultas', '<small class="text-danger">', '</small>'); ?>
                                         </div>
                                         <div class="form-group">
                                             <label class="label" for="prodi">Program Studi</label>
-                                            <input class="form-control" type="text" id="prodi" name="prodi" value="<?= $user['prodi']; ?>">
+                                            <input class="form-control" type="text" id="prodi" name="prodi" value="<?= form_error('prodi') || set_value('prodi') ? set_value('prodi') : $user['prodi']; ?>">
+                                            <?= form_error('prodi', '<small class="text-danger">', '</small>'); ?>
                                         </div>
                                         <div class="form-group">
                                             <label class="label" for="semester">Semester</label>
                                             <select class="form-control" id="semester" name="semester">
                                                 <?php for ($i = 1; $i <= 14; $i++) : ?>
-                                                    <?php if ($user['semester'] == $i) : ?>
+                                                    <?php if ((set_value('semester') != null && set_value('semester') == $i) || (set_value('semester') == null && $user['semester'] == $i)) : ?>
                                                         <option value="<?= $i; ?>" selected><?= $i; ?></option>
                                                     <?php else : ?>
                                                         <option value="<?= $i; ?>"><?= $i; ?></option>
@@ -101,44 +107,90 @@
                                                 <?php endfor; ?>
                                             </select>
                                         </div>
-                                        <button class="btn btn-primary float-right">Simpan perubahan</button>
+                                        <button class="btn btn-primary float-right" id="submitInfoAkad" name="submitInfoAkad" value="submitInfoAkad">Simpan perubahan</button>
                                     </form>
                                 </div>
                             </div>
                         </div>
-                        <div class="tab-pane" id="tab-3" style="height: 550px;">
+                        <div class="tab-pane" id="tab-3" style="max-height: 800px;">
                             <div class="row">
-                                <div class="col-lg-8 details order-2 order-lg-1">
+                                <div class="col-lg-10 details order-2 order-lg-1">
                                     <h3>Berkas</h3>
-                                    <form action="" method="POST">
-                                        <h6>Surat permohonan magang</h6>
-                                        <div class="container" style="border-style: dotted; height: 30%">
-                                            <p class="mt-2">SuratPermohonanMagang.pdf</p>
-                                            <button type="button" class="btn btn-primary btn-sm mb-2">Ubah berkas</button>
+
+                                    <h6>Surat permohonan magang</h6>
+                                    <div id="suratUploadDisplay" class="container" style="border-style: dotted; height: fit-content;">
+                                        <div class="input-group mb-3 mt-3">
+                                            <?= form_open_multipart('pelamar/edit/' . 'suratPermohonan/' . $user['id']) ?>
+                                            <p>Pilih berkas Surat Permohonan Magang anda lalu tekan tombol "Unggah surat".</p>
+                                            <div class="custom-file mb-3">
+                                                <input type="file" class="custom-file-input" id="inputSurat" name="inputSurat">
+                                                <label class="custom-file-label" for="inputSurat">Pilih berkas</label>
+                                            </div>
+                                            <button type="button" class="btn btn-success btn-sm mr-2" id="batal-surat">Batalkan perubahan</button>
+                                            <button type="submit" class="btn btn-primary btn-sm mr-2" id="unggah-surat">Unggah surat</button>
+                                            </form>
                                         </div>
+                                    </div>
 
-                                        <br>
+                                    <div id="suratNormalDisplay" class="container" style="border-style: dotted; height: fit-content;">
+                                        <p class="mt-3"><?= $user['nama_file_surat_permohonan']; ?></p>
+                                        <a class="btn btn-primary btn-sm mb-3 mr-2" href="<?= base_url('pelamar/download/') . 'suratPermohonan/' . $user['id']; ?>">Unduh berkas</a>
+                                        <button type="button" class="btn btn-success btn-sm mb-3 mr-2" id="ubah-surat">Ubah berkas</button>
+                                        <a class="btn btn-danger btn-sm mb-3 mr-2 hapus-berkas" href="<?= base_url('pelamar/delete/')  . 'suratPermohonan/' . $user['id']; ?>">Hapus berkas</a>
+                                    </div>
 
-                                        <h6>Kartu hasil studi</h6>
-                                        <div class="container" style="border-style: dotted; height: 30%">
-                                            <p class="mt-2">KHS.pdf</p>
-                                            <button type="button" class="btn btn-primary btn-sm mb-2">Ubah berkas</button>
+                                    <br>
+
+                                    <h6>Kartu hasil studi</h6>
+                                    <div id="khsUploadDisplay" class="container" style="border-style: dotted; height: fit-content;">
+                                        <div class="input-group mb-3 mt-3">
+                                            <?= form_open_multipart('pelamar/edit/' . 'khs/' . $user['id']) ?>
+                                            <p>Pilih berkas Kartu Hasil Studi anda lalu tekan tombol "Unggah KHS".</p>
+                                            <div class="custom-file mb-3">
+                                                <input type="file" class="custom-file-input" id="inputKhs" name="inputKhs">
+                                                <label class="custom-file-label" for="inputKhs">Pilih berkas</label>
+                                            </div>
+                                            <button type="button" class="btn btn-success btn-sm mr-2" id="batal-khs">Batalkan perubahan</button>
+                                            <button type="submit" class="btn btn-primary btn-sm mr-2" id="unggah-khs">Unggah KHS</button>
+                                            </form>
                                         </div>
+                                    </div>
 
-                                        <br>
+                                    <div id="khsNormalDisplay" class="container" style="border-style: dotted; height: fit-content;">
+                                        <p class="mt-3"><?= $user['nama_file_khs']; ?></p>
+                                        <a class="btn btn-primary btn-sm mb-3 mr-2" href="<?= base_url('pelamar/download/') . 'khs/' . $user['id']; ?>">Unduh berkas</a>
+                                        <button type="button" class="btn btn-success btn-sm mb-3 mr-2" id="ubah-khs">Ubah berkas</button>
+                                        <a class="btn btn-danger btn-sm mb-3 mr-2 hapus-berkas" href="<?= base_url('pelamar/delete/')  . 'khs/' . $user['id']; ?>">Hapus berkas</a>
+                                    </div>
 
-                                        <h6>Curriculum Vitae</h6>
-                                        <div class="container" style="border-style: dotted; height: 30%">
-                                            <p class="mt-2">CV.pdf</p>
-                                            <button type="button" class="btn btn-primary btn-sm mb-2">Ubah berkas</button>
+                                    <br>
+
+                                    <h6>Curriculum Vitae</h6>
+                                    <div id="cvUploadDisplay" class="container" style="border-style: dotted; height: fit-content;">
+                                        <div class="input-group mb-3 mt-3">
+                                            <?= form_open_multipart('pelamar/edit/' . 'cv/' . $user['id']) ?>
+                                            <p>Pilih berkas Curriculum Vitae anda lalu tekan tombol "Unggah CV".</p>
+                                            <div class="custom-file mb-3">
+                                                <input type="file" class="custom-file-input" id="inputCv" name="inputCv">
+                                                <label class="custom-file-label" for="inputCv">Pilih berkas</label>
+                                            </div>
+                                            <button type="button" class="btn btn-success btn-sm mr-2" id="batal-cv">Batalkan perubahan</button>
+                                            <button type="submit" class="btn btn-primary btn-sm mr-2" id="unggah-cv">Unggah CV</button>
+                                            </form>
                                         </div>
+                                    </div>
 
-                                        <button class="btn btn-primary float-right mt-2">Simpan perubahan</button>
-                                    </form>
+                                    <div id="cvNormalDisplay" class="container" style="border-style: dotted; height: fit-content;">
+                                        <p class="mt-3"><?= $user['nama_file_cv']; ?></p>
+                                        <a class="btn btn-primary btn-sm mb-3 mr-2" href="<?= base_url('pelamar/download/') . 'cv/' . $user['id']; ?>">Unduh berkas</a>
+                                        <button type="button" class="btn btn-success btn-sm mb-3 mr-2" id="ubah-cv">Ubah berkas</button>
+                                        <a class="btn btn-danger btn-sm mb-3 mr-2 hapus-berkas" href="<?= base_url('pelamar/delete/')  . 'cv/' . $user['id']; ?>">Hapus berkas</a>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
-       
+
                         <div class="tab-pane" id="tab-4">
                             <div class="row">
                                 <div class="col-lg-8 details order-2 order-lg-1">
@@ -147,7 +199,7 @@
                                 </div>
                             </div>
                         </div>
-       
+
                         <div class="tab-pane" id="tab-5">
                             <div class="row">
                                 <div class="col-lg-8 details order-2 order-lg-1">
--- web-pusdiklat.sql
+++ web-pusdiklat.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 22 Jul 2021 pada 08.34
--- Versi server: 10.4.19-MariaDB
--- Versi PHP: 8.0.7
+-- Waktu pembuatan: 26 Jul 2021 pada 06.29
+-- Versi server: 10.4.20-MariaDB
+-- Versi PHP: 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -57,8 +57,8 @@ CREATE TABLE `login` (
 INSERT INTO `login` (`id`, `email`, `password`, `role_id`) VALUES
 (1, 'Rina_Wahyuni@pusdiklat.com', 'rina123', 1),
 (2, 'PUSDIKLAT@pusdiklat.com', 'pusdiklat', 2),
-(4, 'sleepyweppy@gmail.com', '$2y$10$DdBChV.YU3FTnxPkB79i5.8BE0Vd8IuvHCAK.lpJLn8mJG6OMlT.q', 3),
-(7, 'muhammadrizkyardiansah93@gmail.com', '$2y$10$nW05HXtM1qi4tiUnHZz6l.Hn/9Axhu8.r72.l4.cm3xnhRXPz8Pa.', 3);
+(7, 'muhammadrizkyardiansah93@gmail.com', '$2y$10$nW05HXtM1qi4tiUnHZz6l.Hn/9Axhu8.r72.l4.cm3xnhRXPz8Pa.', 3),
+(10, 'sleepyweppy@gmail.com', '$2y$10$Zlr0yH4J4r0MSUndvS2zp.609Rx19ZuhwVY98ec6Yaw.UihpotWpa', 3);
 
 -- --------------------------------------------------------
 
@@ -87,8 +87,8 @@ CREATE TABLE `pelamar` (
 --
 
 INSERT INTO `pelamar` (`id`, `nama`, `email`, `universitas`, `fakultas`, `prodi`, `nim`, `semester`, `no_telpon`, `no_surat_permohonan`, `nama_file_khs`, `nama_file_cv`, `nama_file_surat_permohonan`) VALUES
-(1, 'Muhammad Rizky', 'sleepyweppy@gmail.com', 'Universitas Yarsi', 'Teknologi Informasi', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(5, 'Muhammad Rizky Ardiansah', 'muhammadrizkyardiansah93@gmail.com', 'Universitas Yarsi', 'Teknologi Informasi', 'Teknik Informatika', '1402018149', 6, '085972875509', 'N123/2/19/888', '8e154e317e9fef7247a3f5884501ec6b.pdf', '29aaa9784599d2f9625d306600127df6.pdf', '028786e02de1746c5ad4dce556497512.pdf');
+(5, 'Muhammad Rizky Ardiansah', 'muhammadrizkyardiansah93@gmail.com', 'Universitas Yarsi', 'Teknologi Informasi', 'Teknik Informatika', '1402018149', 6, '085972875509', 'N123/2/19/888', 'Pelamar_KHS_1627231664.pdf', 'Pelamar_CV_1627233612.pdf', 'Pelamar_SuratPermohonan_1627273458.pdf'),
+(8, 'Madara Sasori', 'sleepyweppy@gmail.com', 'Universitas Negeri Konoha', 'Teknik Ninja', 'Anbu', '123465798', 6, '01010101010', NULL, 'Pelamar_KHS_1627236135.pdf', 'Pelamar_CV_1627236177.pdf', 'Pelamar_SuratPermohonan_1627236071.pdf');
 
 -- --------------------------------------------------------
 
@@ -187,19 +187,19 @@ ALTER TABLE `unit_kerja`
 -- AUTO_INCREMENT untuk tabel `aktivasi`
 --
 ALTER TABLE `aktivasi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT untuk tabel `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT untuk tabel `pelamar`
 --
 ALTER TABLE `pelamar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT untuk tabel `role`
