1c1a75f45805df349addc653f6ebef93e31dfc27
arissetyawan1 <aris.setyawan354313@gmail.com>
2021-07-26 22:03:42 +0700

    Modified auth model

--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -3,93 +3,93 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth_model extends CI_Model
 {
-    public function validateLogin($email, $password)
-    {
-        $result = $this->db->get_where('login', ['email' => $email])->row_array();
-        if ($result != null) {
-            if (password_verify($password, $result['password'])) {
-                return 'berhasil';
-            } else {
-                return 'wrongPass';
-            }
-        } else {
-            return 'emailInv';
-        }
-    }
+	public function validateLogin($email, $password)
+	{
+		$result = $this->db->get_where('login', ['email' => $email])->row_array();
+		if ($result != null) {
+			if (password_verify($password, $result['password'])) {
+				return 'berhasil';
+			} else {
+				return 'wrongPass';
+			}
+		} else {
+			return 'emailInv';
+		}
+	}
 
-    public function getDataLoginByEmail($email)
-    {
-        return $this->db->get_where('login', ['email' => $email])->row_array();
-    }
+	public function getDataLoginByEmail($email)
+	{
+		return $this->db->get_where('login', ['email' => $email])->row_array();
+	}
 
-    public function insertActivationData($data)
-    {
-        $this->db->insert('aktivasi', $data);
-    }
+	public function insertActivationData($data)
+	{
+		$this->db->insert('aktivasi', $data);
+	}
 
-    public function kirimEmail($type, $token, $email)
-    {
-        $config = [
-            'protocol' => 'smtp',
-            'smtp_host' => 'ssl://smtp.googlemail.com',
-            'smtp_user' => 'ci3emailsender@gmail.com',
-            'smtp_pass' => 'satudua3',
-            'smtp_port' => 465,
-            'mailtype' => 'html',
-            'charset' => 'utf-8',
-            'newline' => "\r\n"
-        ];
+	public function kirimEmail($type, $token, $email)
+	{
+		$config = [
+			'protocol' => 'smtp',
+			'smtp_host' => 'ssl://smtp.googlemail.com',
+			'smtp_user' => 'ci3emailsender@gmail.com',
+			'smtp_pass' => 'satudua3',
+			'smtp_port' => 465,
+			'mailtype' => 'html',
+			'charset' => 'utf-8',
+			'newline' => "\r\n"
+		];
 
-        $this->load->library('email', $config);
-        $this->email->initialize($config);
+		$this->load->library('email', $config);
+		$this->email->initialize($config);
 
-        $this->email->from('ci3emailsender@gmail.com', 'CI3 Email Sender');
-        $this->email->to($email);
+		$this->email->from('ci3emailsender@gmail.com', 'CI3 Email Sender');
+		$this->email->to($email);
 
-        if ($type == 'aktivasi') {
-            //send email
-            $this->email->subject('Aktivasi Akun');
-            $this->email->message('Klik tautan berikut untuk mengaktifkan akun anda: <a href="' . base_url('auth/verifikasi') . '?email=' . urlencode($email) . '&token=' . urlencode($token) . '">Aktivasi Sekarang!</a>');
-        } else if ($type == 'forgot_password') {
-            //send email
-            $this->email->subject('Reset Password');
-            $this->email->message('Click this link to reset your password: <a href="' . base_url('auth/resetpassword') . '?email=' . $email . '&token=' . urlencode($token) . '">Reset Password</a>');
-        }
+		if ($type == 'aktivasi') {
+			//send email
+			$this->email->subject('Aktivasi Akun');
+			$this->email->message('Klik tautan berikut untuk mengaktifkan akun anda: <a href="' . base_url('auth/verifikasi') . '?email=' . urlencode($email) . '&token=' . urlencode($token) . '">Aktivasi Sekarang!</a>');
+		} else if ($type == 'forgot_password') {
+			//send email
+			$this->email->subject('Reset Password');
+			$this->email->message('Click this link to reset your password: <a href="' . base_url('auth/resetpassword') . '?email=' . $email . '&token=' . urlencode($token) . '">Reset Password</a>');
+		}
 
-        if ($this->email->send()) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+		if ($this->email->send()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    public function verifikasiAkun($email, $token)
-    {
-        $result = $this->db->get_where('aktivasi', ['email' => $email])->row_array();
-        if ($result != null) {
-            if ($result['token'] == $token) {
-                $dataPelamar = [
-                    "nama" => $result['nama'],
-                    "email" => $result['email'],
-                    "universitas" => $result['universitas'],
-                    "fakultas" => $result['fakultas']
-                ];
-                $this->db->insert('pelamar', $dataPelamar);
+	public function verifikasiAkun($email, $token)
+	{
+		$result = $this->db->get_where('aktivasi', ['email' => $email])->row_array();
+		if ($result != null) {
+			if ($result['token'] == $token) {
+				$dataPelamar = [
+					"nama" => $result['nama'],
+					"email" => $result['email'],
+					"universitas" => $result['universitas'],
+					"fakultas" => $result['fakultas']
+				];
+				$this->db->insert('pelamar', $dataPelamar);
 
-                $dataLogin = [
-                    "email" => $result['email'],
-                    "password" => $result['password'],
-                    "role_id" => 1
-                ];
-                $this->db->insert('login', $dataLogin);
+				$dataLogin = [
+					"email" => $result['email'],
+					"password" => $result['password'],
+					"role_id" => 3
+				];
+				$this->db->insert('login', $dataLogin);
 
-                $this->db->delete('aktivasi', ['email' => $email]);
-                return 'berhasil';
-            } else {
-                return 'tokenInv';
-            }
-        } else {
-            return 'emailInv';
-        }
-    }
+				$this->db->delete('aktivasi', ['email' => $email]);
+				return 'berhasil';
+			} else {
+				return 'tokenInv';
+			}
+		} else {
+			return 'emailInv';
+		}
+	}
 }
