78f5f516be3ee431469e5dc8568c6290f67522dc
arissetyawan1 <aris.setyawan354313@gmail.com>
2021-08-15 23:40:20 +0700

    modified balasan_model.php

--- application/models/Balasan_model.php
+++ application/models/Balasan_model.php
@@ -11,7 +11,7 @@ class Balasan_model extends CI_Model
 		$this->db->from("surat_permohonan");
 		$this->db->join('pelamar', 'surat_permohonan.id_pelamar = pelamar.id', 'LEFT');
 		$this->db->join('unit_kerja', 'surat_permohonan.id_unit = unit_kerja.id', 'LEFT');
-		$this->db->join('surat_balasan', 'surat_permohonan.id_permohonan = surat_balasan.id_surat_mohon', 'LEFT');
+		$this->db->join('surat_balasan', 'surat_permohonan.id_permohonan = surat_balasan.id_surat_permohonan', 'LEFT');
 		$this->db->where('status !=', $status['status']);
 		return $this->db->get()->result_array();
 	}
@@ -41,7 +41,7 @@ class Balasan_model extends CI_Model
 		$this->db->from("surat_permohonan");
 		$this->db->join('pelamar', 'surat_permohonan.id_pelamar = pelamar.id', 'LEFT');
 		$this->db->join('unit_kerja', 'surat_permohonan.id_unit = unit_kerja.id', 'LEFT');
-		$this->db->join('surat_balasan', 'surat_permohonan.id_permohonan = surat_balasan.id_surat_mohon', 'LEFT');
+		$this->db->join('surat_balasan', 'surat_permohonan.id_permohonan = surat_balasan.id_surat_permohonan', 'LEFT');
 		$this->db->where('status !=', 'Menunggu Verifikasi');
 		$this->db->where('id_surat_balasan =', null);
 		return $this->db->count_all_results();
